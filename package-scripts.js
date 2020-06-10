a68c2d69895bfcfd232defd7bc19fab4f0c3a6ec
YuseiAbe <yabe@diverta.co.jp>
2020-06-10 18:01:18 +0900

    add standalone

--- package-scripts.js
+++ package-scripts.js
@@ -2,7 +2,7 @@ const frameworks = ["angular", "react", "react-ts", "vue", "vue-ts", "jquery"];
 
 module.exports = {
   scripts: {
-    'update': `kuroco pull && kuroco generate -o kuroco -l js`,
+    'update': `kuroco pull && kuroco generate -o kuroco -l js --lib && kuroco generate -o kuroco-standalone -l js --standalone`,
     ...frameworks.reduce(
       (prev, cur) => ({
         ...prev,
