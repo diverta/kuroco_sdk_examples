7c21b76980519c90a3077510094d6c159f2366a7
YuseiAbe <yabe@diverta.co.jp>
2020-06-10 18:01:00 +0900

    add standalone

---
+++ examples/jquery-standalone-ex/public/main.js
@@ -0,0 +1,31 @@
+const { Auth, TopicsService } = Kuroco;
+
+$(() => {
+  getTopicsList()
+    .then(res => {
+      const topicsIDsMsg = res.list.map(({ topics_id }) => topics_id).join("\n");
+      renderMsg(topicsIDsMsg);
+    })
+    .catch(err => {
+      renderMsg(
+        ["Oops! something error was occured!", err.toString()].join("\n")
+      );
+    });
+});
+
+function renderMsg(msg) {
+  const $content = $("#content");
+  $content.fadeOut("slow", () => {
+    $content
+      .text(msg)
+      .css("color", "red")
+      .fadeIn("slow");
+  });
+}
+
+async function getTopicsList() {
+  await Auth.login({
+    requestBody: { email: 'test', password: 'qwer1234' },
+  })
+  return await TopicsService.getTopicsServiceRcmsApi1Topics1({});
+}
